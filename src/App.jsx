4d7eb5be85b2fb120118870e5e7a444e5781c1df
shubhamkaundal98 <shubham.kaundal98@gmail.com>
2024-06-03 01:42:37 +0530

    change width dimensions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="h-full flex flex-wrap content-between font-sans container">
+    <div className="h-full max-w-full flex flex-wrap content-between font-sans container">
       <div className="w-full block content">
         <Header />
         <main>
