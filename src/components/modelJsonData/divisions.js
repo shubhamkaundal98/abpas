584d56177ef26d246bc32fcf498dd5f36f9ad6cf
shubhamkaundal98 <shubham.kaundal98@gmail.com>
2024-06-07 02:27:59 +0530

    Add model json data

---
+++ src/components/modelJsonData/divisions.js
@@ -0,0 +1,12 @@
+export const divisions = [
+  { id: "Bhopal", name: "Bhopal" },
+  { id: "Chambal", name: "Chambal" },
+  { id: "Gwalior", name: "Gwalior" },
+  { id: "Indore", name: "Indore" },
+  { id: "Jabalpur", name: "Jabalpur" },
+  { id: "Narmadapuram", name: "Narmadapuram" },
+  { id: "Rewa", name: "Rewa" },
+  { id: "Sagar", name: "Sagar" },
+  { id: "Shahdol", name: "Shahdol" },
+  { id: "Ujjain", name: "Ujjain" },
+];
