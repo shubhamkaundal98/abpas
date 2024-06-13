dac3b3f0bedc53dd497679e84b2f3468d9b32922
shubhamkaundal98 <shubham.kaundal98@gmail.com>
2024-06-13 13:28:57 +0530

    feat: Add Controller (useControl) for Granular Control

--- src/components/formComponents/SignupForm.jsx
+++ src/components/formComponents/SignupForm.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -9,41 +9,41 @@ export default function SignupForm({
   handleCloseSignupModal,
   handleOpenSignupRenewalModal,
 }) {
-  const { register, handleSubmit } = useForm();
+  const { handleSubmit, control } = useForm();
   const navigate = useNavigate();
 
   const showData = (data) => {
-    if (data.new) {
+    if (data.signup_type === "new") {
       navigate("/signup");
-    } else if (data.renewal) {
-      // handleCloseSignupModal();
-      // handleOpenSignupRenewalModal();
+    } else if (data.signup_type === "renewal") {
+      handleCloseSignupModal();
+      handleOpenSignupRenewalModal();
     }
   };
 
   return (
     <form onSubmit={handleSubmit(showData)}>
       <div className="flex gap-10">
-        <FormControl>
-          <RadioGroup
-            row
-            aria-labelledby="row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
-          >
-            <FormControlLabel
-              {...register("new")}
-              value="new"
-              control={<Radio />}
-              label="New"
-            />
-            <FormControlLabel
-              {...register("renewal")}
-              value="renewal"
-              control={<Radio />}
-              label="Renewal"
-            />
-          </RadioGroup>
-        </FormControl>
+        <Controller
+          control={control}
+          name="signup_type"
+          render={({ field }) => (
+            <FormControl {...field}>
+              <RadioGroup
+                row
+                aria-labelledby="row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+              >
+                <FormControlLabel value="new" control={<Radio />} label="New" />
+                <FormControlLabel
+                  value="renewal"
+                  control={<Radio />}
+                  label="Renewal"
+                />
+              </RadioGroup>
+            </FormControl>
+          )}
+        />
       </div>
       <div className="bg-slate-200 py-[0.5px] my-5 "></div>
       <div className="flex justify-end">
